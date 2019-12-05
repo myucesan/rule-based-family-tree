cf84f58ef0c93838dced98d7112e28943701fce2
= <=>
2019-12-05 17:49:58 +0100

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,95 @@
+import com.github.cschen1205.ess.engine.*;
+
+public class Main {
+    private static RuleInferenceEngine getInferenceEngine()
+    {
+        RuleInferenceEngine rie=new KieRuleInferenceEngine();
+
+        Rule rule=new Rule("Bicycle");
+        rule.addAntecedent(new EqualsClause("vehicleType", "cycle"));
+        rule.addAntecedent(new EqualsClause("num_wheels", "2"));
+        rule.addAntecedent(new EqualsClause("motor", "no"));
+        rule.setConsequent(new EqualsClause("vehicle", "Bicycle"));
+        rie.addRule(rule);
+
+        rule=new Rule("Tricycle");
+        rule.addAntecedent(new EqualsClause("vehicleType", "cycle"));
+        rule.addAntecedent(new EqualsClause("num_wheels", "3"));
+        rule.addAntecedent(new EqualsClause("motor", "no"));
+        rule.setConsequent(new EqualsClause("vehicle", "Tricycle"));
+        rie.addRule(rule);
+
+        rule=new Rule("Motorcycle");
+        rule.addAntecedent(new EqualsClause("vehicleType", "cycle"));
+        rule.addAntecedent(new EqualsClause("num_wheels", "2"));
+        rule.addAntecedent(new EqualsClause("motor", "yes"));
+        rule.setConsequent(new EqualsClause("vehicle", "Motorcycle"));
+        rie.addRule(rule);
+
+        rule=new Rule("SportsCar");
+        rule.addAntecedent(new EqualsClause("vehicleType", "automobile"));
+        rule.addAntecedent(new EqualsClause("size", "medium"));
+        rule.addAntecedent(new EqualsClause("num_doors", "2"));
+        rule.setConsequent(new EqualsClause("vehicle", "Sports_Car"));
+        rie.addRule(rule);
+
+        rule=new Rule("Sedan");
+        rule.addAntecedent(new EqualsClause("vehicleType", "automobile"));
+        rule.addAntecedent(new EqualsClause("size", "medium"));
+        rule.addAntecedent(new EqualsClause("num_doors", "4"));
+        rule.setConsequent(new EqualsClause("vehicle", "Sedan"));
+        rie.addRule(rule);
+
+        rule=new Rule("MiniVan");
+        rule.addAntecedent(new EqualsClause("vehicleType", "automobile"));
+        rule.addAntecedent(new EqualsClause("size", "medium"));
+        rule.addAntecedent(new EqualsClause("num_doors", "3"));
+        rule.setConsequent(new EqualsClause("vehicle", "MiniVan"));
+        rie.addRule(rule);
+
+        rule=new Rule("SUV");
+        rule.addAntecedent(new EqualsClause("vehicleType", "automobile"));
+        rule.addAntecedent(new EqualsClause("size", "large"));
+        rule.addAntecedent(new EqualsClause("num_doors", "4"));
+        rule.setConsequent(new EqualsClause("vehicle", "SUV"));
+        rie.addRule(rule);
+
+        rule=new Rule("Cycle");
+        rule.addAntecedent(new LessClause("num_wheels", "4"));
+        rule.setConsequent(new EqualsClause("vehicleType", "cycle"));
+        rie.addRule(rule);
+
+        rule=new Rule("Automobile");
+        rule.addAntecedent(new EqualsClause("num_wheels", "4"));
+        rule.addAntecedent(new EqualsClause("motor", "yes"));
+        rule.setConsequent(new EqualsClause("vehicleType", "automobile"));
+        rie.addRule(rule);
+
+       return rie;
+    }
+
+    public static void testForwardChain()
+    {
+        RuleInferenceEngine rie=getInferenceEngine();
+        rie.addFact(new EqualsClause("num_wheels", "4"));
+        rie.addFact(new EqualsClause("motor", "yes"));
+        rie.addFact(new EqualsClause("num_doors", "3"));
+        rie.addFact(new EqualsClause("size", "medium"));
+
+        System.out.println("before inference");
+        System.out.println(rie.getFacts());
+        System.out.println();
+
+        rie.infer(); //forward chain
+
+        System.out.println("after inference");
+        System.out.println(rie.getFacts());
+        System.out.println();
+    }
+
+    public static void 
+
+    public static void main(String[] args) {
+        testForwardChain();
+    }
+}
