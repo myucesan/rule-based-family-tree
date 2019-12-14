96b9a71feb997e5597c81b4c3508a08201c9e98f
= <=>
2019-12-14 12:18:25 +0100

    finished

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# rule-based-family-tree
-# rule-based-family-tree
+# Expert System Practicum
+
+500769574 | Mustafa Yücesan | [mustafa.yucesan@hva.nl](mailto:mustafa.yucesan@hva.nl)
+
+# Introductie
+
+Voor het implementeren van het expert systeem ben ik gebonden aan de mogelijkheden van deze java shell. In deze shell kan ik meerdere antecendenten (condities) toevoegen en als er 5 antecedenten zijn praten we over 5 AND condities. Deze shell doet niet aan OR. Veel mensen gebruiken PROLOG voor het expert systeem waarbij ik voor de verandering voor een java expert systeem wilde kiezen. Ik zal kijken in hoeverre het mogelijk is om een familieboom in deze shell te maken.
+
+Deze shell biedt zowel forward chaining als backward chaining. Forward chaining houdt in dat het langzamerhand van A naar B gaat maar bij backward chaining is het juist andersom waarbij het B als doel stelt en langzaam terugwerkt van B naar A. Dit blijkt in de praktijk iets efficiënter te werken.
+
+Voor dit practicum zijn een aantal vragen gesteld. Deze vragen lijken gebaseerd te zijn op het gebruik van PROLOG maar ik zal ze zo goed mogelijk proberen te beantwoorden met behulp van deze expert shell.
+
+## Vraag - en antwoord
+
+---
+
+Maak een voorbeeld systeem met je eigen "familieboom" als data en regels over vader, moeder, oom, neef/nicht (cousin en nephew)
+en laat het systeem vragen beantwoorden als "wie zijn (klein-)zonenbeschrijf/test hoe het werkt. Let op dat je geen feiten toevoegd die het systeem zelf af kan leiden dus hou de initiële kennisbank zo klein mogelijk.
+
+Er is een methode dat gebruik maakt van de RuleInferenceEngine klasse dat wordt gebruikt om regels aan feiten te koppelen. In deze methode worden er regels aan de database toegevoegd. Daarna wordt er een andere functie aangemaakt die gebruik maakt van de inference engine, 
+daar wordt een variabele met de inference engine als datatype. De returnwaarde van de RuleInferenceEngine methode (de returnwaarde is de engine met database) wordt gegeven aan dit variabelen. In de tweede methode met de variabele worden 'facts' toegevoegd. Het vreemde van deze expert shell is dat het maar tot 1 resultaat kan komen. Oftewel, op het moment dat een rule matcht aan de beschikbare feiten dan krijg je direct resultaat. Dat is ook wel logisch maar
+in het geval van een familieboom zou je wel verder moeten kijken. Het is niet direct een IF THEN gebaseerd systeem wat het een beetje vaag maakt en lastig om een familieboom expert systeem van te maken. Ik zal relatie namen gebruiken voor de familieboom in plaats van namen in verband met privacygevoel.
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,80 +1,70 @@
 import com.github.cschen1205.ess.engine.*;
 
+import java.util.Vector;
+
 public class Main {
-    private static RuleInferenceEngine getInferenceEngine()
+
+    private static RuleInferenceEngine getInferenceEngineFamilyTree()
     {
         RuleInferenceEngine rie=new KieRuleInferenceEngine();
 
-        Rule rule=new Rule("Bicycle");
-        rule.addAntecedent(new EqualsClause("vehicleType", "cycle"));
-        rule.addAntecedent(new EqualsClause("num_wheels", "2"));
-        rule.addAntecedent(new EqualsClause("motor", "no"));
-        rule.setConsequent(new EqualsClause("vehicle", "Bicycle"));
+        Rule rule=new Rule("Father");
+        rule.addAntecedent(new EqualsClause("hasChildren", "yes"));
+        rule.addAntecedent(new EqualsClause("gender", "male"));
+        rule.setConsequent(new EqualsClause("relation", "Father"));
         rie.addRule(rule);
 
-        rule=new Rule("Tricycle");
-        rule.addAntecedent(new EqualsClause("vehicleType", "cycle"));
-        rule.addAntecedent(new EqualsClause("num_wheels", "3"));
-        rule.addAntecedent(new EqualsClause("motor", "no"));
-        rule.setConsequent(new EqualsClause("vehicle", "Tricycle"));
+        rule=new Rule("Mother");
+        rule.addAntecedent(new EqualsClause("hasChildren", "yes"));
+        rule.addAntecedent(new EqualsClause("gender", "female"));
+        rule.setConsequent(new EqualsClause("relation", "Mother"));
         rie.addRule(rule);
 
-        rule=new Rule("Motorcycle");
-        rule.addAntecedent(new EqualsClause("vehicleType", "cycle"));
-        rule.addAntecedent(new EqualsClause("num_wheels", "2"));
-        rule.addAntecedent(new EqualsClause("motor", "yes"));
-        rule.setConsequent(new EqualsClause("vehicle", "Motorcycle"));
+        rule=new Rule("Daughter");
+        rule.addAntecedent(new EqualsClause("gender", "female"));
+        rule.addAntecedent(new EqualsClause("hasParent", "yes"));
+        rule.setConsequent(new EqualsClause("relation", "daughter"));
         rie.addRule(rule);
 
-        rule=new Rule("SportsCar");
-        rule.addAntecedent(new EqualsClause("vehicleType", "automobile"));
-        rule.addAntecedent(new EqualsClause("size", "medium"));
-        rule.addAntecedent(new EqualsClause("num_doors", "2"));
-        rule.setConsequent(new EqualsClause("vehicle", "Sports_Car"));
+        rule=new Rule("Son");
+        rule.addAntecedent(new EqualsClause("gender", "male"));
+        rule.addAntecedent(new EqualsClause("hasParent", "yes"));
+        rule.setConsequent(new EqualsClause("relation", "son"));
         rie.addRule(rule);
 
-        rule=new Rule("Sedan");
-        rule.addAntecedent(new EqualsClause("vehicleType", "automobile"));
-        rule.addAntecedent(new EqualsClause("size", "medium"));
-        rule.addAntecedent(new EqualsClause("num_doors", "4"));
-        rule.setConsequent(new EqualsClause("vehicle", "Sedan"));
+        rule=new Rule("Brother");
+        rule.addAntecedent(new EqualsClause("hasSibling", "yes"));
+        rule.addAntecedent(new EqualsClause("gender", "male"));
+        rule.setConsequent(new EqualsClause("relation", "Brother"));
         rie.addRule(rule);
 
-        rule=new Rule("MiniVan");
-        rule.addAntecedent(new EqualsClause("vehicleType", "automobile"));
-        rule.addAntecedent(new EqualsClause("size", "medium"));
-        rule.addAntecedent(new EqualsClause("num_doors", "3"));
-        rule.setConsequent(new EqualsClause("vehicle", "MiniVan"));
+        rule=new Rule("Sister");
+        rule.addAntecedent(new EqualsClause("hasSibling", "yes"));
+        rule.addAntecedent(new EqualsClause("gender", "female"));
+        rule.setConsequent(new EqualsClause("relation", "Sister"));
         rie.addRule(rule);
 
-        rule=new Rule("SUV");
-        rule.addAntecedent(new EqualsClause("vehicleType", "automobile"));
-        rule.addAntecedent(new EqualsClause("size", "large"));
-        rule.addAntecedent(new EqualsClause("num_doors", "4"));
-        rule.setConsequent(new EqualsClause("vehicle", "SUV"));
+
+        rule=new Rule("Uncle");
+        rule.addAntecedent(new EqualsClause("hasSibling", "yes"));
+        rule.addAntecedent(new EqualsClause("hasNibling", "yes"));
+        rule.setConsequent(new EqualsClause("relation", "Uncle/Aunt"));
         rie.addRule(rule);
 
-        rule=new Rule("Cycle");
-        rule.addAntecedent(new LessClause("num_wheels", "4"));
-        rule.setConsequent(new EqualsClause("vehicleType", "cycle"));
+        rule=new Rule("Nibling");
+        rule.addAntecedent(new EqualsClause("hasUncleAndOrAunt", "yes"));
+        rule.setConsequent(new EqualsClause("relation", "Nibling"));
         rie.addRule(rule);
 
-        rule=new Rule("Automobile");
-        rule.addAntecedent(new EqualsClause("num_wheels", "4"));
-        rule.addAntecedent(new EqualsClause("motor", "yes"));
-        rule.setConsequent(new EqualsClause("vehicleType", "automobile"));
-        rie.addRule(rule);
-
-       return rie;
+        return rie;
     }
 
-    public static void testForwardChain()
+    public static void doInferFC()
     {
-        RuleInferenceEngine rie=getInferenceEngine();
-        rie.addFact(new EqualsClause("num_wheels", "4"));
-        rie.addFact(new EqualsClause("motor", "yes"));
-        rie.addFact(new EqualsClause("num_doors", "3"));
-        rie.addFact(new EqualsClause("size", "medium"));
+        RuleInferenceEngine rie=getInferenceEngineFamilyTree();
+        rie.addFact(new EqualsClause("gender", "female"));
+        rie.addFact(new EqualsClause("hasSibling", "yes"));
+        rie.addFact(new EqualsClause("hasParent", "yes"));
 
         System.out.println("before inference");
         System.out.println(rie.getFacts());
@@ -87,9 +77,26 @@ public class Main {
         System.out.println();
     }
 
-    public static void 
+    public static void doInferBC()
+    {
+        RuleInferenceEngine rie=getInferenceEngineFamilyTree();
+        rie.addFact(new EqualsClause("gender", "female"));
+        rie.addFact(new EqualsClause("hasSibling", "yes"));
+        rie.addFact(new EqualsClause("hasParent", "yes"));
+
+        System.out.println("Infer: family relation");
+
+        Vector<Clause> unproved_conditions= new Vector<Clause>();
+
+        Clause conclusion=rie.infer("relation", unproved_conditions);
+
+        System.out.println("Conclusion: "+conclusion);
+    }
+
 
     public static void main(String[] args) {
-        testForwardChain();
+        doInferBC();
+        System.out.println("_____________________________________________________________________________________________________________________________________________________________________________");
+
     }
 }
